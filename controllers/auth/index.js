ed87b9ba3b89d8c39e14c217fab7a5ff7cdecc22
Ihor Karen <karenihor@gmail.com>
2023-08-25 12:16:47 +0300

    fix

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -4,11 +4,12 @@ const { register } = require("./register");
 const { login } = require("./login");
 const { logout } = require("./logout");
 const { getCurrent } = require("./current");
+const { theme } = require("../user/theme");
 
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   logout: ctrlWrapper(logout),
   getCurrent: ctrlWrapper(getCurrent),
+  theme: ctrlWrapper(theme),
 };
-
--- controllers/user/index.js
+++
@@ -1,7 +0,0 @@
-const { ctrlWrapper } = require("../../helpers");
-
-const { theme } = require("./theme");
-
-module.exports = {
-  theme: ctrlWrapper(theme),
-};
