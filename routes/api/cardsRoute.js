f56cdddde92dde46f72df7e6dcdb466e86455692
Ihor Karen <karenihor@gmail.com>
2023-08-27 16:35:36 +0300

    replace routes in code

--- routes/api/cardsRoute.js
+++ routes/api/cardsRoute.js
@@ -5,14 +5,14 @@ const { schemas } = require("../../models/board/board");
 
 const router = express.Router();
 
-router.put("/:boardId", authenticate, ctrl.updateCard);
-
 router.post(
   "/:boardId",
   validateBody(schemas.addCard),
   authenticate,
   ctrl.addCard
 );
+
+router.put("/:boardId", authenticate, ctrl.updateCard);
 
 router.patch("/:boardId", authenticate, ctrl.replaceCard);
 
