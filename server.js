89c6edcd9d84f5b28abd5538ef52caa4eeb9b934
Ihor Karen <karenihor@gmail.com>
2023-09-07 18:37:15 +0300

    automatic wake-up

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const http = require("http");
 const app = require("./app");
 
 const { DB_HOST } = process.env;
@@ -10,6 +11,15 @@ mongoose
   .then(() => {
     app.listen(3000);
     console.log("Database connection successful");
+
+    const interval = 600000;
+    setInterval(() => {
+      http
+        .get("https://task-pro-covc.onrender.com/", (res) => {})
+        .on("error", (err) => {
+          console.error(err.message);
+        });
+    }, interval);
   })
   .catch((error) => {
     console.log(error.message);
